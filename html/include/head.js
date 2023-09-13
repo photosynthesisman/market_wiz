4c362223f8ddb168ce2d4b7a02c7ab353890ad15
js choi <photosynthesis0131@gmail.com>
2023-09-13 12:45:08 +0900

    [퍼블] 택 작업 중

--- html/include/head.js
+++ html/include/head.js
@@ -13,9 +13,9 @@
   <script type="text/javascript" src="${loot}static/js/lib/jquery-ui.1.13.2.min.js"></script>
   <script type="text/javascript" src="${loot}static/js/lib/lodash.4.17.21.min.js"></script>
   <script type="text/javascript" src="${loot}static/js/ui-plugins.min.js"></script>  
-  <script type="text/javascript" src="${loot}static/js/ui-util.js"></script>
-  <script type="module" src="${loot}static/js/main.js"></script>
-  <script type="text/javascript" src="${loot}static/js/ui-front.js"></script>`;
+  <script type="module" src="${loot}static/js/main.js"></script>`;
+  // <script type="text/javascript" src="${loot}static/js/ui-util.js"></script>
+  // <script type="text/javascript" src="${loot}static/js/ui-front.js"></script>`;
   document.write(str);
   const $include = document.querySelector(".__include");
   if ($include) $include.remove();
