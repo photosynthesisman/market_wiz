700df4a0c6c6818a562624c80bd86e764fdaf125
js choi <photosynthesis0131@gmail.com>
2023-09-13 14:02:26 +0900

    [퍼블] 탭 정리

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,36 @@
 import { includeHTML } from "./layout.js";
 
 includeHTML();
+
+$(".market").on("click", ".tab a", function () {
+  $(this).parent().siblings().removeClass("active");
+  $(".tab-content").removeClass("active");
+  $(this).parent().addClass("active");
+  let tabId = $(this).data("tab");
+  $("#" + tabId).addClass("active");
+});
+
+$(".market").on("click", ".close-tab", function () {
+  const clikedTab = $(this).prev("a").attr("data-tab");
+  const onlyActive = $("#" + clikedTab);
+  onlyActive.removeClass("active");
+  $(this).parents("li").remove();
+});
+
+$(".market").on("click", ".folding-panel li", function () {
+  const tab_name = $(this).text();
+  const data_name = $(this).data("name");
+  $(".tab-container ul").append(
+    '<li class="tab"><a href="#" data-tab="' +
+      data_name +
+      '">' +
+      tab_name +
+      '</a><span class="close-tab">닫기</span></li>'
+  );
+});
+
+$(".market").on("click", ".folding-btn", function () {
+  $(this).parent().next(".folding-panel").stop().slideToggle(300);
+  $(this).parents("li").toggleClass("open").siblings().removeClass("open");
+  $(this).parents("li").siblings().find(".folding-panel").slideUp(300);
+});
