1ddc99cba2edb921b364d9ebefe38f8f51113966
js choi <photosynthesis0131@gmail.com>
2023-09-13 18:09:19 +0900

    [퍼블] 스크립트 정리

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,8 @@
+// header&lnb 불러오기
 import { includeHTML } from "./layout.js";
-
 includeHTML();
 
+// 탭메뉴 및 패널 활성화
 $(".market").on("click", ".tab a", function () {
   $(this).parent().siblings().removeClass("active");
   $(".tab-content").removeClass("active");
@@ -10,6 +11,7 @@ $(".market").on("click", ".tab a", function () {
   $("#" + tabId).addClass("active");
 });
 
+// 탭메뉴 닫기
 $(".market").on("click", ".close-tab", function () {
   const clikedTab = $(this).prev("a").attr("data-tab");
   const onlyActive = $("#" + clikedTab);
@@ -17,6 +19,7 @@ $(".market").on("click", ".close-tab", function () {
   $(this).parents("li").remove();
 });
 
+// lnb 메뉴 클릭시 탭 생성
 $(".market").on("click", ".folding-panel li", function () {
   const tab_name = $(this).text();
   const data_name = $(this).data("name");
@@ -29,8 +32,21 @@ $(".market").on("click", ".folding-panel li", function () {
   );
 });
 
+// 탭 메뉴 닫기
 $(".market").on("click", ".folding-btn", function () {
   $(this).parent().next(".folding-panel").stop().slideToggle(300);
   $(this).parents("li").toggleClass("open").siblings().removeClass("open");
   $(this).parents("li").siblings().find(".folding-panel").slideUp(300);
+});
+
+// 페이지 로드
+$("#collect-site-product").load("tab2content.html");
+
+// lnb 접기/펼치기
+$(".market").on("click", ".close-lnb", function () {
+  if ($(this).parents(".lnb-inner").hasClass("fold")) {
+    $(this).parents(".lnb-inner").removeClass("fold");
+  } else {
+    $(this).parents(".lnb-inner").addClass("fold");
+  }
 });
