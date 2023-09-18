1484d043412ad6d0f07950017cc5aa4c0d3e98ea
js choi <photosynthesis0131@gmail.com>
2023-09-18 14:11:54 +0900

    [퍼블] SO 공통 작업 중

--- static/js/main.js
+++ static/js/main.js
@@ -49,6 +49,7 @@ $(".market").on("click", ".folding-btn", function (e) {
 });
 
 // 페이지 로드
+$("#font-guide").load("font-guide.html");
 $("#dashboard").load("dashboard.html");
 $("#collect-site-product").load("collect-site-product.html");
 
@@ -68,4 +69,62 @@ $(".market").on("click", "#showPopup", function () {
 
 $(".market").on("click", "#closePopup", function () {
   $("#popup, #overlay").fadeOut();
+});
+
+//전체 선택
+$(".market").on("click", "#chk_all", function () {
+  const checkItem = $(".item-wrap").find("input[type=checkbox]");
+  if (!$(this).prop("checked")) {
+    checkItem.prop("checked", false);
+  } else {
+    checkItem.prop("checked", true);
+  }
+});
+$(".market").on("click", ".item input[type=checkbox]", function () {
+  const chkAll = $("#chk_all");
+  const siblingCheck = $(this).parents(".item").siblings().find("input[type=checkbox]");
+  if (siblingCheck.length === siblingCheck.filter(":checked").length && $(this).prop("checked")) {
+    chkAll.prop("checked", true);
+  } else {
+    chkAll.prop("checked", false);
+  }
+});
+
+// pagination
+$(document).ready(function () {
+  $("#dashboard").load("dashboard.html", function () {
+    const itemsPerPage = 10;
+    const content = $(".item-wrap");
+    const pagination = document.getElementById("pagination");
+
+    function showPage(pageNumber) {
+      const startIndex = (pageNumber - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      const items = [...content.children()];
+      items.forEach((item, index) => {
+        if (index >= startIndex && index < endIndex) {
+          item.style.display = "flex";
+        } else {
+          item.style.display = "none";
+        }
+      });
+    }
+
+    function createPaginationLinks() {
+      const totalItems = content.children().length;
+      const totalPages = Math.ceil(totalItems / itemsPerPage);
+      for (let i = 1; i <= totalPages; i++) {
+        const li = document.createElement("li");
+        li.textContent = i;
+        li.addEventListener("click", () => showPage(i));
+        pagination.appendChild(li);
+      }
+    }
+    createPaginationLinks();
+    showPage(1);
+    $("#pagination li:first-child").addClass("active");
+    $("#pagination li").click(function () {
+      $(this).addClass("active").siblings().removeClass("active");
+    });
+  });
 });
