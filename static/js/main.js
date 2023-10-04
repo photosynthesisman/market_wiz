de68d495c85da670c25a8c88e77096ac112a8b5b
js choi <photosynthesis0131@gmail.com>
2023-10-04 17:15:37 +0900

    [퍼블] 소스 정리

--- static/js/main.js
+++ static/js/main.js
@@ -2,97 +2,79 @@
 import { includeHTML } from "./layout.js";
 includeHTML();
 
-// 탭메뉴 및 패널 활성화
-$(".market").on("click", ".tab a", function (e) {
-  e.preventDefault();
-  $(this).parent().siblings().removeClass("active");
-  $(".tab-content").removeClass("active");
-  $(this).parent().addClass("active");
-  let tabId = $(this).data("tab");
-  $("#" + tabId).addClass("active");
-});
-
-// 탭메뉴 닫기
-$(".market").on("click", ".close-tab", function () {
-  const clikedTab = $(this).prev("a").attr("data-tab");
-  const onlyActive = $("#" + clikedTab);
-  onlyActive.removeClass("active");
-  $(this).parents("li").remove();
-});
-
-// lnb 메뉴 클릭시 탭 생성
-$(".market").on("click", ".lnb-menu", function () {
-  var clickedText = $(this)
-    .contents()
-    .filter(function () {
-      return this.nodeType == 3;
-    })
-    .text();
-  const tab_name = clickedText;
-  const data_name = $(this).data("name");
-  $(".tab-container ul").append(
-    '<li class="tab"><a href="#" data-tab="' +
-      data_name +
-      '">' +
-      tab_name +
-      '</a><span class="close-tab">닫기</span></li>'
-  );
-});
-
-// lnb 하위 뎁스 열기
-$(".market").on("click", ".folding-btn", function (e) {
-  e.preventDefault();
-  $(this).parent().next(".folding-panel").stop().slideToggle(300);
-  $(this).parents("li").toggleClass("open").siblings().removeClass("open");
-  $(this).parents("li").siblings().find(".folding-panel").slideUp(300);
-});
-
-// lnb 접기/펼치기
-$(".market").on("click", ".close-lnb", function () {
-  if ($(this).parents(".lnb-inner").hasClass("fold")) {
-    $(this).parents(".lnb-inner").removeClass("fold");
-  } else {
-    $(this).parents(".lnb-inner").addClass("fold");
-  }
-});
-
-//pooup 오픈/클로즈
-$(".market").on("click", ".open-pop", function () {
-  const popId = $(this).attr("data-name");
-  $("#" + popId).fadeIn();
-});
-$(".market").on("click", ".close", function () {
-  $(this).parents(".popup").fadeOut();
-});
-$(".market").on("click", ".closePopup", function () {
-  if ($(this).parents(".popup").hasClass("open")) {
-    return false;
-  } else {
-    $(this).parents(".popup").fadeOut();
-  }
-});
-
-//전체 선택
-$(".market").on("click", "[id^=chk_all]", function () {
-  const checkItem = $(".item-wrap").find("input[type=checkbox]");
-  if (!$(this).prop("checked")) {
-    checkItem.prop("checked", false);
-  } else {
-    checkItem.prop("checked", true);
-  }
-});
-$(".market").on("click", ".item input[type=checkbox]", function () {
-  const chkAll = $("[id^=chk_all]");
-  const siblingCheck = $(this).parents(".item").siblings().find("input[type=checkbox]");
-  if (siblingCheck.length === siblingCheck.filter(":checked").length && $(this).prop("checked")) {
-    chkAll.prop("checked", true);
-  } else {
-    chkAll.prop("checked", false);
-  }
-});
-
-//validation
 $(document).ready(function () {
+  // 탭메뉴 및 패널 활성화
+  $(".market").on("click", ".tab a", function (e) {
+    e.preventDefault();
+    $(this).parent().siblings().removeClass("active");
+    $(".tab-content").removeClass("active");
+    $(this).parent().addClass("active");
+    let tabId = $(this).data("tab");
+    $("#" + tabId).addClass("active");
+  });
+
+  // lnb 2depth open
+  $(".market").on("click", ".folding-btn", function (e) {
+    e.preventDefault();
+    $(this).parent().next(".folding-panel").stop().slideToggle(300);
+    $(this).parents("li").toggleClass("open").siblings().removeClass("open");
+    $(this).parents("li").siblings().find(".folding-panel").slideUp(300);
+  });
+  // lnb 3depth open
+  $(".market").on("click", ".has-depth", function () {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this).find("ul").fadeOut();
+    } else {
+      $(this).addClass("active");
+      $(this).find("ul").fadeIn();
+    }
+  });
+
+  // lnb 접기/펼치기
+  $(".market").on("click", ".close-lnb", function () {
+    if ($(this).parents(".lnb-inner").hasClass("fold")) {
+      $(this).parents(".lnb-inner").removeClass("fold");
+    } else {
+      $(this).parents(".lnb-inner").addClass("fold");
+    }
+  });
+
+  //pooup 오픈/클로즈
+  $(".market").on("click", ".open-pop", function () {
+    const popId = $(this).attr("data-name");
+    $("#" + popId).fadeIn();
+  });
+  $(".market").on("click", ".close", function () {
+    $(this).parents(".popup").fadeOut();
+  });
+  $(".market").on("click", ".closePopup", function () {
+    if ($(this).parents(".popup").hasClass("open")) {
+      return false;
+    } else {
+      $(this).parents(".popup").fadeOut();
+    }
+  });
+
+  //전체 선택
+  $(".market").on("click", "[id^=chk_all]", function () {
+    const checkItem = $(".item-wrap").find("input[type=checkbox]");
+    if (!$(this).prop("checked")) {
+      checkItem.prop("checked", false);
+    } else {
+      checkItem.prop("checked", true);
+    }
+  });
+  $(".market").on("click", ".item input[type=checkbox]", function () {
+    const chkAll = $("[id^=chk_all]");
+    const siblingCheck = $(this).parents(".item").siblings().find("input[type=checkbox]");
+    if (siblingCheck.length === siblingCheck.filter(":checked").length && $(this).prop("checked")) {
+      chkAll.prop("checked", true);
+    } else {
+      chkAll.prop("checked", false);
+    }
+  });
+
   //count number of count
   $(document).on("input", ".txt-area", function (e) {
     const counterEle = $(this).next();
@@ -122,24 +104,16 @@ $(document).ready(function () {
     $(this).parent().children("li:not(.init)").toggle();
     $(this).parents(".select-box").removeClass("active");
   });
-});
-$(".market").on("click", ".has-depth", function () {
-  if ($(this).hasClass("active")) {
-    $(this).removeClass("active");
-    $(this).find("ul").fadeOut();
-  } else {
-    $(this).addClass("active");
-    $(this).find("ul").fadeIn();
-  }
-});
-//tooltip 관련
-$(".market").on("mouseover", ".tooltip", function () {
-  $(this).next().stop().show();
-});
-$(".market").on("mouseout", ".tooltip", function () {
-  $(this).next().stop().hide();
-});
-//favorite 관련
-$(".market").on("click", ".favorite", function () {
-  $(this).hasClass("checked") ? $(this).removeClass("checked") : $(this).addClass("checked");
+
+  //tooltip 관련
+  $(".market").on("mouseover", ".tooltip", function () {
+    $(this).next().stop().show();
+  });
+  $(".market").on("mouseout", ".tooltip", function () {
+    $(this).next().stop().hide();
+  });
+  //favorite 관련
+  $(".market").on("click", ".favorite", function () {
+    $(this).hasClass("checked") ? $(this).removeClass("checked") : $(this).addClass("checked");
+  });
 });
